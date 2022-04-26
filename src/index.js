6886b2a7d2797ecd7e11f70a50328da9f1db6bf6
brionychiu <yuikuta0618@gmail.com>
2022-04-26 11:57:13 +0800

    20220426 - setup webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom"; 
+console.log('hello');
+
+let app = <div>hello react jsx</div>;
+ReactDOM.render(app,document.getElementById('root'));
